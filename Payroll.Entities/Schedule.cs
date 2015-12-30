40a50089d16755b9afa510ee3f221562bdaa1c7c
Jona Ross Pereira <pereirajonaross@gmail.com>
2015-12-30 21:15:38 +0800

    Entity for Employee Schedule

---
+++ Payroll.Entities/Schedule.cs
@@ -0,0 +1,25 @@
+﻿using Payroll.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payroll.Entities
+{
+    [Table("tbl_schedule")]
+    public class Schedule
+    {
+        [Key]
+        public int AttendanceId { get; set; }
+
+        public int StartDay { get; set; }
+
+        public int EndDay { get; set; }
+
+        public TimePeriod Timeperiod { get; set; }
+
+    }
+}
