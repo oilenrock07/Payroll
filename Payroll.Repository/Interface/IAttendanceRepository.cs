7d607f929809048441636bee559016e638ccfc65
Jona Ross Pereira <pereirajonaross@gmail.com>
2016-02-08 21:32:16 +0800

    [New] - Initial commit for Employee Hours generation

--- Payroll.Repository/Interface/IAttendanceRepository.cs
+++ Payroll.Repository/Interface/IAttendanceRepository.cs
@@ -1,10 +1,14 @@
 ﻿using Payroll.Entities;
 using Payroll.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
 
 namespace Payroll.Repository.Interface
 {
     public interface IAttendanceRepository : IRepository<Attendance>
     {
         Attendance GetLastAttendance(string employeeCode);
+
+        IList<Attendance> GetAttendanceByDateRange(string employeeCode, DateTime fromDate, DateTime toDate);
     }
 }
