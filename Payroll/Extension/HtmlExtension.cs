6b13aad0c2a6214adb9744257f47c71005c868dc
Cornelio Cawicaan <cornelio@solutionists.co.nz>
2016-03-06 15:01:35 +0800

    MVC Datepicker extension

---
+++ Payroll/Extension/HtmlExtension.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Payroll.Extension
+{
+    public static class HtmlExtension
+    {
+        public static MvcHtmlString DatePickerFor(this HtmlHelper htmlHelper, string name, DateTime? value, object htmlAttributes)
+        {
+            var textbox = new TagBuilder("input");
+            textbox.Attributes.Add("type", "text");
+            textbox.Attributes.Add("name", name);
+            textbox.Attributes.Add("id", name);
+
+            if (value != null)
+            {
+                textbox.Attributes.Add("value", Convert.ToDateTime(value).ToString("MM/dd/yyyy"));
+            }
+
+            textbox.MergeAttributes(new RouteValueDictionary(htmlAttributes));
+
+            string html = String.Format("<div class=\"input-group\">{0}<label class=\"input-group-addon\" for=\"{1}\"><i class=\"fa fa-calendar\"></i></label></div>", textbox.ToString(TagRenderMode.Normal), name);
+            return MvcHtmlString.Create(html);
+        }
+
+        public static MvcHtmlString DatePickerFor(this HtmlHelper htmlHelper, string name, string value, object htmlAttributes)
+        {
+            var textbox = new TagBuilder("input");
+            textbox.Attributes.Add("type", "text");
+            textbox.Attributes.Add("name", name);
+            textbox.Attributes.Add("id", name);
+
+            if (!string.IsNullOrEmpty(value))
+            {
+                textbox.Attributes.Add("value", value);
+            }
+
+            textbox.MergeAttributes(new RouteValueDictionary(htmlAttributes));
+
+            string html = String.Format("<div class=\"input-group\">{0}<label class=\"input-group-addon\" for=\"{1}\"><i class=\"fa fa-calendar\"></i></label></div>", textbox.ToString(TagRenderMode.Normal), name);
+            return MvcHtmlString.Create(html);
+        }
+    }
+}
--- Payroll/Scripts/app.js
+++ Payroll/Scripts/app.js
@@ -1,7 +1,12 @@
 ﻿$(function() {
 
+    function handleChangeDate() {
+        $(this).datepicker('hide');
+    }
+
     function init() {
         $('.datepicker').datepicker({ format: 'mm/dd/yyyy' });
+        $('.datepicker').on('changeDate', handleChangeDate);
     }
 
     init();
