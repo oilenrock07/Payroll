6897ae6afc5758011322d6d98ec3e797b7dd8627
Cornelio Cawicaan <cornelio@solutionists.co.nz>
2016-05-31 22:03:28 +0800

    [Jona] - Initial payroll schedule

---
+++ Payroll.Scheduler/Payroll.Scheduler/Schedules/EmployeePayrollSchedule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Payroll.Entities.Payroll;
+using Payroll.Repository.Interface;
+using Payroll.Repository.Repositories;
+using Payroll.Scheduler.Interfaces;
+
+namespace Payroll.Scheduler.Schedules
+{
+    public class EmployeePayrollSchedule : ISchedule
+    {
+        private readonly IEmployeeHoursRepository _employeeHoursRepository;
+        private readonly ITotalEmployeeHoursRepository _totalEmployeeHoursRepository;
+        private readonly IEmployeeDepartmentRepository _employeeDepartmentRepository;
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly IAttendanceRepository _attendanceRepository;
+        private readonly IAttendanceLogRepository _attendanceLogRepository;
+
+        public EmployeePayrollSchedule()
+        {
+        }
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
