c68bb23858e31c756f78af1400830b482375455b
CAWI\User <cawicaancornelio@gmail.com>
2017-03-16 22:16:33 +0800

    Mark Holiday in Attendance

--- Payroll/Models/Attendance/AttendanceBreakdownViewModel.cs
+++ Payroll/Models/Attendance/AttendanceBreakdownViewModel.cs
@@ -7,5 +7,8 @@ namespace Payroll.Models.Attendance
         public double RegularHours { get; set; }
         public double Overtime { get; set; }
         public double NightDifferential { get; set; }
+
+        public bool IsHoliday { get; set; }
+        public bool IsRegularHoliday { get; set; }
     }
 }
