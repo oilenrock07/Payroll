5da4dc69b084a47934152fe97998d69dda45bb64
Jona Ross Pereira <pereirajonaross@gmail.com>
2017-05-29 21:27:37 +0800

    Employee payroll per company employee hours initial

---
+++ Payroll.Repository/Interface/IEmployeePayrollItemPerCompanyRepository.cs
@@ -0,0 +1,19 @@
+﻿using Payroll.Entities.Enums;
+using Payroll.Entities.Payroll;
+using Payroll.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payroll.Repository.Interface
+{
+    public interface IEmployeePayrollItemPerCompanyRepository : 
+        IRepository<EmployeePayrollItemPerCompany>
+    {
+        EmployeePayrollItemPerCompany Find(int employeeId, DateTime date, RateType rateType);
+
+        IList<EmployeePayrollItemPerCompany> GetByDateRange(DateTime dateFrom, DateTime dateTo);
+    }
+}
---
+++ Payroll.Service/Interfaces/ITotalEmployeeHoursPerCompanyService.cs
@@ -0,0 +1,21 @@
+﻿using Payroll.Entities.Enums;
+using Payroll.Entities.Payroll;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payroll.Service.Interfaces
+{
+   public interface ITotalEmployeeHoursPerCompanyService
+    {
+        IList<TotalEmployeeHoursPerCompany> GetByDateRange(DateTime dateFrom, DateTime dateTo);
+
+        IList<TotalEmployeeHoursPerCompany> GetByTypeAndDateRange(int employeeId, RateType? rateType, DateTime payrollStartDate, DateTime payrollEndDate);
+
+        IList<TotalEmployeeHoursPerCompany> GetByEmployeeDate(int employeeId, DateTime date);
+
+        double CountTotalHours(int employeeId, DateTime date);
+    }
+}
